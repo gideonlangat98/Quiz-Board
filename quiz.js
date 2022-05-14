f1157a9aea545c88e8c3b2349a6c4d7410d0fcd2
gideonlangat98 <gideonlangat98@gmail.com>
2022-05-14 22:54:46 +0300

    it will add js functions

--- quiz.js
+++ quiz.js
@@ -1,5 +1,6 @@
 function check(){
-    var A=0;
+
+    var a = 0;
     var q1 = document.quizzes.question1.value;
     var q2 = document.quizzes.question2.value;
     var q3 = document.quizzes.question3.value;
@@ -10,25 +11,36 @@ function check(){
     var q8 = document.quizzes.question8.value;
     var q9 = document.quizzes.question9.value;
     var q10 = document.quizzes.question10.value;
+    var score=document.getElementById("score");
+    var quizzes=document.getElementById("quizzes")
+   
 
-    if (q1=="It is an inbuilt language that adds interactivity to the user") {A++} 
-    if (q2=="Css") {A++}
-    if (q3=="Objects") {A++}
-    if (q4=="&&") {A++}
-    if (q5=="Select") {A++}
-    if (q6=="Variables") {A++}
-    if (q7=="To join two or more things together") {A++}
-    if (q8==".length") {A++}
-    if (q9=="Absolute") {A++}
-    if (q10=="Alert box") {A++}
 
-        document.write(A);
+    if (q1=="It is an inbuilt language that adds interactivity to the user") {a++} 
+    if (q2=="Css") {a++}
+    if (q3=="Objects") {a++}
+    if (q4=="&&") {a++}
+    if (q5=="Select") {a++}
+    if (q6=="Variables") {a++}
+    if (q7=="To join two or more things together") {a++}
+    if (q8==".length") {a++}
+    if (q9=="Absolute") {a++}
+    if (q10=="Alert box") {a++}
+
+   quizzes.style.display="none";
+ 
+   if(a<=3){
+       score.textContent=`Your score is ${a}.Poor Score!`
+   }
+   if (a ){
+       score.textContent=`Your score is ${a}. Fairly passed.`
+   } else{
+       score.textContent=`Your score is ${a} .Excellent!`
+   } 
+
                     
                     
-                    
-                    
-                       
-                 
+                             
             
         
 }
