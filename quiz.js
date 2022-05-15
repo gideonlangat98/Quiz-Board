73f71615511b26e851077573672c61c84d72b532
gideonlangat98 <gideonlangat98@gmail.com>
2022-05-15 14:22:20 +0300

    it will make changes on quiz.js file

--- quiz.js
+++ quiz.js
@@ -1,6 +1,8 @@
+
+
+// UI Logic //
 function check(){
 
-    var a = 0;
     var q1 = document.quizzes.question1.value;
     var q2 = document.quizzes.question2.value;
     var q3 = document.quizzes.question3.value;
@@ -11,37 +13,52 @@ function check(){
     var q8 = document.quizzes.question8.value;
     var q9 = document.quizzes.question9.value;
     var q10 = document.quizzes.question10.value;
-    var score=document.getElementById("score");
-    var quizzes=document.getElementById("quizzes")
+    
+    var correct = 0;
+    if (q1=="It is an inbuilt language that adds interactivity to the user") 
+       {correct++}
+       document.write(correct);
+
+    if (q2=="Css") 
+      {correct++}
+       document.write(correct);
+
+
+    if (q3=="Objects") 
+       {correct++}
+       document.write(correct);
+
+    if (q4=="&&") 
+       {correct++}
+       document.write(correct);
+
+
+    if (q5=="Select") 
+       document.write(correct);
+
+    if (q6=="Variables") 
+      {correct++}
+       document.write(correct);
+
+    if (q7=="To join two or more things together") 
+      {correct++}
+      document.write(correct);
+
+    if (q8==".length") 
+      {correct++}
+      document.write(correct);
+
+    if (q9=="Absolute") 
+      {correct++}
+      document.write(correct);
+
+    if (q10=="Alert box")
+      {correct++}
+      document.write(correct);
+
+
    
-
-
-    if (q1=="It is an inbuilt language that adds interactivity to the user") {a++} 
-    if (q2=="Css") {a++}
-    if (q3=="Objects") {a++}
-    if (q4=="&&") {a++}
-    if (q5=="Select") {a++}
-    if (q6=="Variables") {a++}
-    if (q7=="To join two or more things together") {a++}
-    if (q8==".length") {a++}
-    if (q9=="Absolute") {a++}
-    if (q10=="Alert box") {a++}
-
-   quizzes.style.display="none";
- 
-   if(a<=3){
-       score.textContent=`Your score is ${a}.Poor Score!`
-   }
-   if (a ){
-       score.textContent=`Your score is ${a}. Fairly passed.`
-   } else{
-       score.textContent=`Your score is ${a} .Excellent!`
-   } 
-
-                    
-                    
-                             
-            
+                      
         
 }
 
